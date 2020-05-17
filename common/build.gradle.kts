00cdb1654f640d43896c7f98eb5aab1f380cf2f1
mignatov <ignatovmv@profi.ru>
2020-05-17 23:11:57 +0300

    remove tests

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
-    //id("co.touchlab.native.cocoapods")
-    //id("kotlinx-serialization")
+    // id("co.touchlab.native.cocoapods")
+    // id("kotlinx-serialization")
     id("com.android.library")
-    //id("com.squareup.sqldelight")
+    // id("com.squareup.sqldelight")
 }
 
 android {
@@ -13,5 +15,28 @@ android {
         targetSdkVersion(Versions.targetSdk)
         versionCode = 1
         versionName = "1.0"
+    }
+}
+
+kotlin {
+    android()
+    // Revert to just ios() when gradle plugin can properly resolve it
+    val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
+    if (onPhone) {
+        iosArm64("ios")
+    } else {
+        iosX64("ios")
+    }
+    targets.getByName<KotlinNativeTarget>("ios").compilations["main"].kotlinOptions.freeCompilerArgs +=
+        listOf("-Xobjc-generics", "-Xg0")
+
+    version = "1.1"
+
+    sourceSets["commonMain"].dependencies {
+        implementation(kotlin("stdlib-common", Versions.kotlin))
+    }
+
+    sourceSets["androidMain"].dependencies {
+        implementation(kotlin("stdlib", Versions.kotlin))
     }
 }
